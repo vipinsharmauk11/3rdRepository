1fd76c51558d8baad58f320a7c69268811fc8e25
vipinsharma935 <142110379+vipinsharma935@users.noreply.github.com>
2024-06-25 15:55:08 +0530

    Add 2nd Programme file named GitProgramme2.java in GitHub

---
+++ src/pkg1/GitProgramme2.java
@@ -0,0 +1,10 @@
+package pkg1;
+
+public class GitProgramme2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Hello GitHub.. This is git Programme2 ..");
+	}
+
+}
