9a9b93d31551f8494322ce2b054705fd3cb7f9c4
vipinsharmauk11 <vipinsharmauk11@gmail.com>
2024-06-25 16:08:33 +0530

    Update GitProgramme1.java

--- src/pkg1/GitProgramme1.java
+++ src/pkg1/GitProgramme1.java
@@ -5,6 +5,8 @@ public class GitProgramme1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("Hello GitHub.. This is git Programme1...");
+      System.out.println("Hello GitHub.. This is git Programme1...");
+		
 	}
 
 }
